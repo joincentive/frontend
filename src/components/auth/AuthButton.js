2d0c4de19c56b516b4175b39c18f3ab1000250ba
jackyzha0 <j.zhao2k19@gmail.com>
2021-09-18 12:43:21 -0500

    prisma files

--- src/components/auth/AuthButton.js
+++ src/components/auth/AuthButton.js
@@ -1,7 +1,7 @@
-import { useSession, signIn, signOut } from "next-auth/client"
+import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function AuthButton() {
-  const [session, loading] = useSession()
+  const { data: session } = useSession()
   if (session) {
     return (
       <>
