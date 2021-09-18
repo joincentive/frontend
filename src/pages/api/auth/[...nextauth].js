bc6e693faf19771420926d04b8d11c47ec7fbb98
jackyzha0 <j.zhao2k19@gmail.com>
2021-09-18 18:17:49 -0500

    portal finalization

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,36 +1,18 @@
-<<<<<<< HEAD
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
-=======
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
->>>>>>> 17ebd4abc27e812aa26a0e76665870aee68aa431
+
 
 const prisma = new PrismaClient()
 
 export default NextAuth({
-<<<<<<< HEAD
-	adapter: PrismaAdapter(prisma),
-	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_ID,
-			clientSecret: process.env.GITHUB_SECRET,
-			scope: 'write:repo_hook,read:user',
-		}),
-	],
-})
-=======
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: "write:repo_hook,read:user"
+      scope: 'write:repo_hook,read:user',
     }),
   ],
 })
->>>>>>> 17ebd4abc27e812aa26a0e76665870aee68aa431
