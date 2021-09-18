b9bae7b641016c387dd4d01376fab9f68997a049
Anson Yu <ansonyu24@gmail.com>
2021-09-18 12:48:02 -0400

    style: refactored to use style-jsx

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+	return (
+		<>
+			<p>Centive lets community compensate creators</p>
+			<style jsx>{`
+				color: white;
+				position: relative;
+				z-index: 3;
+			`}</style>
+		</>
+	)
+}
+
+export default Hero
