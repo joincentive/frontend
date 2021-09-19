869666f3388ca4dc4d607ed21e010c3d4105235b
jackyzha0 <j.zhao2k19@gmail.com>
2021-09-18 20:22:31 -0500

    remove hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Generative from '../components/Generative'
-import Hero from '../components/hero'
 import Projects from '../components/Projects'
 import Button from '../components/Button'
 
