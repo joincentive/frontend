ff671a65f26d0d09852142d8b6a8d4cb0ba724ba
Anson Yu <ansonyu24@gmail.com>
2021-09-18 20:52:26 -0400

    content: added home and about page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,25 +1,17 @@
 import React from 'react'
 
-const Hero = () => {
+const Projects = () => {
 	return (
 		<div>
-			<h1>Centive lets community compensate creators</h1>
+			<h1>Latest projects</h1>
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh eu risus ut amet at montes,
 				faucibus sit. Non massa lacus, fames in quis malesuada in vitae convallis. Vestibulum et
 				tincidunt faucibus maecenas turpis egestas posuere sed. Ut porttitor cursus est sit sagittis
 				donec adipiscing neque a.
 			</p>
-
-			<style jsx>{`
-				display: flex;
-				flex-direction: column;
-				justify-content: right;
-				margin-top: 0px;
-				z-index: 10;
-			`}</style>
 		</div>
 	)
 }
 
-export default Hero
+export default Projects
